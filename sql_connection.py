f1c2fbf6012f19e997836e53b9a7c2bee1ad0197
olegchekar98 <34788019+olegchekar98@users.noreply.github.com>
2018-02-05 09:51:34 +0200

    Add files via upload

--- sql_connection.py
+++ sql_connection.py
@@ -1,1 +1,38 @@
+from __future__ import print_function
+import mysql.connector
+from mysql.connector import errorcode
 
+
+cnx = mysql.connector.connect(user='root', password='password', host='127.0.0.1',database='my_connection')
+cursor = cnx.cursor()
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+
+
+TABLES = {}
+TABLES['crypto_table'] = (
+    "CREATE TABLE `crypto_table` ("
+    "  `name` varchar(20) NOT NULL,"
+    "  `price` double(50) NOT NULL,"
+    "  PRIMARY KEY (`name`)")
+
+add_crypto = ("INSERT INTO crypto_table "
+              "(name, price) "
+              "VALUES (%(name)s, %(price)s)")
+
+!!!!!
+data_crypto = {
+  'name': naming,
+  'price': pricing,
+}
+!!!!!
+cursor.execute(add_crypto, data_crypto)
+cnx.commit()
+cursor.close()
+cnx.close()
