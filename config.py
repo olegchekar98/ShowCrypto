8d212572a3f16013ea92db013e8fea90242488b1
olegchekar98 <34788019+olegchekar98@users.noreply.github.com>
2018-02-06 20:10:14 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -5,20 +5,17 @@ import urllib.request
 
 
 class CryptoCurrency:
-    def __init__(self, x):
+    def __init__(self, x, naming, pricing):
         url = urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=10")
         json_ported = json.load(url)
         self.json_ported = json_ported
          # x is a number in json massive from API
         self.x = x
-
+        self.naming = naming
+        self.printing = pricing
+        self.naming = self.json_ported[self.x]['name']
+        self.printing = self.json_ported[self.x]['price_usd']
 
     def printing(self):
         print(self.json_ported[self.x]['name'])
         print(self.json_ported[self.x]['price_usd'])
-
-    def naming(self):
-        return self.json_ported[self.x]['name']
-
-    def pricing(self):
-        return self.json_ported[self.x]['price_usd']
