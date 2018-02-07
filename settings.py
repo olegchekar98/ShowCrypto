ead7fc7b44fda3d0cdacab3a5e24c6cc19e3d313
olegchekar98 <34788019+olegchekar98@users.noreply.github.com>
2018-02-07 13:15:11 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 from config import CryptoCurrency
 
 crypto_dict = {
-    'bitcoin' : CryptoCurrency(0),
-    'ethereum' : CryptoCurrency(1),
-    'ripple' : CryptoCurrency(2),
+    'bitcoin' : CryptoCurrency(0, 0, 0),
+    'ethereum' : CryptoCurrency(1, 0, 0),
+    'ripple' : CryptoCurrency(2, 0, 0)
 }
 
 
