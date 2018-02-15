120512aa94875503ff6b35a4b5ec0b78ab8e9394
olegchekar98 <34788019+olegchekar98@users.noreply.github.com>
2018-02-15 15:25:32 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from config import CryptoCurrency
+from schedule_create import PaintSchedule
 
 crypto_dict = {
     'bitcoin' : CryptoCurrency(0, 0, 0),
@@ -6,6 +7,11 @@ crypto_dict = {
     'ripple' : CryptoCurrency(2, 0, 0)
 }
 
+schedule_val = {
+'bitc' : PaintSchedule('bitcoin'),
+'eth' : PaintSchedule('ethereum'),
+'rip' : PaintSchedule('ripple')
+}
 
 
 
